96d1f8df54025b1cff3ddfd76dc38891320050af
phucpham <phamhuuphuc4049@gmail.com>
2025-02-02 10:46:44 +0700

    fix(layout): changing banner color

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
     return (
-        <div className="h-56 bg-emerald-700 flex flex-col justify-center gap-y-5 items-center text-white">
+        <div className="h-56 bg-blue-900 flex flex-col justify-center gap-y-5 items-center text-white">
             <h2 className="text-6xl font-bold">VBLOG</h2>
             <p className="text-2xl">A place to share your knowledge.</p>
         </div>
