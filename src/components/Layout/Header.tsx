5a2cd2a78630c764efedb2382335b0d8b896d5bc
phucpham <phamhuuphuc4049@gmail.com>
2025-02-05 10:35:45 +0700

    feat(navigation): linking homepage with signin/signup

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,20 +2,25 @@
 import React from 'react'
 import LoginIcon from '@mui/icons-material/Login'
 import HowToRegIcon from '@mui/icons-material/HowToReg'
+import Link from 'next/link'
 const Header = () => {
     return (
         <div className="flex justify-between h-20 px-24 bg-yellow-400">
             <h1 className="place-self-center text-3xl font-bold text-blue-700 cursor-pointer">
-                VBLOG
+                <Link href={'/'}>VBLOG</Link>
             </h1>
             <ul className="flex gap-x-4 place-self-center text-xl">
                 <li className="border-2 border-blue-400 px-2 py-1 rounded-md cursor-pointer bg-blue-200">
-                    <LoginIcon />
-                    Sign in
+                    <Link href="/signin">
+                        <LoginIcon />
+                        Sign in
+                    </Link>
                 </li>
                 <li className="border-2 border-blue-400 px-2 py-1 rounded-md cursor-pointer bg-blue-200">
-                    <HowToRegIcon />
-                    Sign up
+                    <Link href={'/signup'}>
+                        <HowToRegIcon />
+                        Sign up
+                    </Link>
                 </li>
             </ul>
         </div>
