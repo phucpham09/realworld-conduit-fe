b8fa884c8e3a1e6c52e2a078e193a1e755e4194e
phucpham <phamhuuphuc4049@gmail.com>
2025-02-22 23:05:02 +0700

    fix(signup&signin): building with mobile screen

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -23,32 +23,33 @@ const SignIn = () => {
         <div className="h-[calc(100vh-10rem)] flex justify-center items-center">
             <div className="h-3/4 sm:w-1/3 w-3/4 flex flex-col items-center justify-center sm:px-10 px-6 border-2 border-blue-700 bg-gray-100 rounded-lg gap-y-10">
                 <h1 className="text-3xl font-semibold">Sign In</h1>
-                <form className="w-full" onSubmit={handleSubmit}>
-                    <fieldset className="flex flex-col">
-                        <label className="text-2xl">Email</label>
-                        <input
-                            onChange={(e) => setEmail(e.target.value)}
-                            value={email}
-                            type="email"
-                            className="border-2 mb-6 px-3 py-2 text-2xl rounded-md mt-2"
-                            placeholder="Enter your email..."
-                        />
-                        <label className="text-2xl">Password</label>
-                        <input
-                            onChange={(e) => setPassword(e.target.value)}
-                            value={password}
-                            type="password"
-                            className="border-2 mb-3 px-3 py-2 text-2xl rounded-md mt-2"
-                            placeholder="Enter your password..."
-                        />
-                        <p className="text-red-600 italic ">{noti}</p>
-                        <button
-                            type="submit"
-                            className="border py-2 text-xl font-semibold mb-4 place-self-end px-4 text-white bg-blue-700 border-blue-700 rounded-lg"
-                        >
-                            Submit
-                        </button>
-                    </fieldset>
+                <form
+                    className="w-11/12 flex flex-col "
+                    onSubmit={handleSubmit}
+                >
+                    <label className="text-2xl">Email</label>
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        type="email"
+                        className="border-2 mb-6 px-3 py-2 sm:text-2xl text-xl rounded-md mt-2"
+                        placeholder="Enter your email..."
+                    />
+                    <label className="text-2xl">Password</label>
+                    <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        type="password"
+                        className="border-2 mb-3 px-3 py-2 sm:text-2xl text-xl rounded-md mt-2"
+                        placeholder="Enter your password..."
+                    />
+                    <p className="text-red-600 italic ">{noti}</p>
+                    <button
+                        type="submit"
+                        className="border py-2 text-xl font-semibold mb-4 place-self-end px-4 text-white bg-blue-700 border-blue-700 rounded-lg"
+                    >
+                        Submit
+                    </button>
                 </form>
             </div>
             <ToastContainer autoClose={1500} />
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -15,41 +15,39 @@ const SignUp = () => {
     }
     return (
         <div className="h-[calc(100vh-10rem)] flex justify-center items-center">
-            <div className="w-1/3 flex flex-col items-center justify-center px-10 pt-4 border-2 border-blue-700 bg-gray-100 rounded-lg gap-y-10">
+            <div className="sm:w-1/3 w-3/4 flex flex-col items-center justify-center sm:px-10 px-6 pt-4 border-2 border-blue-700 bg-gray-100 rounded-lg gap-y-10">
                 <h1 className="text-3xl font-semibold">Sign Up</h1>
-                <form className="w-full" onSubmit={handleSubmit}>
-                    <fieldset className="flex flex-col">
-                        <label className="text-2xl">Username</label>
-                        <input
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            type="text"
-                            className="border-2 mb-6 px-3 py-2 text-2xl rounded-md mt-2"
-                            placeholder="Enter your username..."
-                        />
-                        <label className="text-2xl">Email</label>
-                        <input
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            type="email"
-                            className="border-2 mb-6 px-3 py-2 text-2xl rounded-md mt-2"
-                            placeholder="Enter your email..."
-                        />
-                        <label className="text-2xl">Password</label>
-                        <input
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            type="password"
-                            className="border-2 mb-6 px-3 py-2 text-2xl rounded-md mt-2"
-                            placeholder="Enter your password..."
-                        />
-                        <button
-                            type="submit"
-                            className="border py-2 text-xl font-semibold mb-4 place-self-end px-4 text-white bg-blue-700 border-blue-700 rounded-lg"
-                        >
-                            Submit
-                        </button>
-                    </fieldset>
+                <form className="w-11/12 flex flex-col" onSubmit={handleSubmit}>
+                    <label className="text-2xl">Username</label>
+                    <input
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        type="text"
+                        className="border-2 mb-6 px-3 py-2 sm:text-2xl text-xl rounded-md mt-2"
+                        placeholder="Enter your username..."
+                    />
+                    <label className="text-2xl">Email</label>
+                    <input
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        className="border-2 mb-6 px-3 py-2 sm:text-2xl text-xl rounded-md mt-2"
+                        placeholder="Enter your email..."
+                    />
+                    <label className="text-2xl">Password</label>
+                    <input
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                        className="border-2 mb-6 px-3 py-2 sm:text-2xl text-xl rounded-md mt-2"
+                        placeholder="Enter your password..."
+                    />
+                    <button
+                        type="submit"
+                        className="border py-2 text-xl font-semibold mb-4 place-self-end px-4 text-white bg-blue-700 border-blue-700 rounded-lg"
+                    >
+                        Submit
+                    </button>
                 </form>
             </div>
         </div>
